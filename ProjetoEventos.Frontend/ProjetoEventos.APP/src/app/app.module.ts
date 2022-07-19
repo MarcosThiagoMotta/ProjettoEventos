1b50a66a982f0e85bc055642402d32840a22acba
Marcos Thiago <mottamarcosthiago@gmail.com>
2022-07-18 23:52:22 -0300

    Desfazendo as cagadas, ou não

--- ProjetoEventos.Frontend/ProjetoEventos.APP/src/app/app.module.ts
+++ ProjetoEventos.Frontend/ProjetoEventos.APP/src/app/app.module.ts
@@ -1,26 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EventosComponent } from './eventos/eventos.component';
-import { PalestrantesComponent } from './palestrantes/palestrantes.component';
-
-import { HttpClientModule } from '@angular/common/http';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    EventosComponent,
-    PalestrantesComponent,
-
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    BrowserAnimationsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
